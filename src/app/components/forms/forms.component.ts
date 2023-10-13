98e61d6b5d130f692c0f8466e1f0d23a784f0c0f
Elizeu Lima <elizeucoca@gmail.com>
2023-10-12 21:42:57 -0300

    criando form group

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-forms',
@@ -7,11 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsComponent implements OnInit {
 
-  results = [];
+  results: Location[] = [];
 
-  constructor(){}
+  formGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){}
+
 
   ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    })
+  }
 
+  onSubmit(): void {
+    console.log(this.formGroup.value);
+  }
+
+  onClean(): void {
+    console.log(this.formGroup.value);
   }
 }
